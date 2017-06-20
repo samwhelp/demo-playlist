31ae071336ad4e54068ae6f9e6da3d4147be25a0
samwhelp <samwhelp@users.noreply.github.com>
2017-06-20 13:52:33 +0800

    add

---
+++ all/debian-packaging-workshop/update.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl http://127.0.0.1:4000/demo-playlist/dyn/debian-packaging-workshop/main.m3u > main.m3u
---
+++ all/guts/update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl http://127.0.0.1:4000/demo-playlist/dyn/guts/arm.m3u > arm.m3u
+curl http://127.0.0.1:4000/demo-playlist/dyn/guts/dykc.m3u > dykc.m3u
+curl http://127.0.0.1:4000/demo-playlist/dyn/guts/homework.m3u > homework.m3u
+curl http://127.0.0.1:4000/demo-playlist/dyn/guts/oscar.m3u > oscar.m3u
+curl http://127.0.0.1:4000/demo-playlist/dyn/guts/vm.m3u > vm.m3u
